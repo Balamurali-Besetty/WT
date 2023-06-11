d1ae821c8d5e6de122de734e06417bc5993a0197
Balamurali-Besetty <98407708+Balamurali-Besetty@users.noreply.github.com>
2023-06-11 21:46:04 +0530

    Delete Flask Project directory

--- Flask Project/Readme.md
+++
@@ -1,1 +0,0 @@
-Its a project on shopping cart developed using Flask 
--- Flask Project/app.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask , render_template , session
-app = Flask(__name__)
-@app.route('/')
-  def home():
-    return render_template('índex.html')
